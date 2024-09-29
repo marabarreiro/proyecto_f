c3875ac5e77a5f0d3a6f38c3ffb45c38b172b210
Mara Barreiro Alonso <mara.barreiro.alonso@rai.usc.es>
2024-09-29 17:44:00 +0200

    Mi segundo commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from lib import cuadrado
 print("Proyecto Figuras")
 lado=4
-print(f"El aread de un cuadrado de lado {lado} es: {cuadrado.get_area(lado)}")
+print(f"El area de un cuadrado de lado {lado} es: {cuadrado.get_area(lado)}")
